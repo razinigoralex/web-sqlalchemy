87a15d84c46423f8d702a6327ccfb9724e2ece98
Igor <razinigoralex@yandex.ru>
2021-03-16 22:32:26 +0300

    job_api: method to get one job is done

--- api/jobs_api.py
+++ api/jobs_api.py
@@ -17,3 +17,18 @@ def get_jobs():
             'jobs': [item.to_dict() for item in jobs]
         }
     )
+
+
+@blueprint.route('/api/news/<int:job_id>', methods=['GET'])
+def get_asked_job(job_id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(job_id)
+
+    if not job:
+        return flask.jsonify({'error': 'Ничего не найдено'})
+
+    return flask.jsonify(
+        {
+            'job': job.to_dict()
+        }
+    )
